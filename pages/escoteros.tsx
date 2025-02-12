65786b6b4882f0181a2521158dd5492721e73749
DAVIDGITHU-githu-david <parapirata5@gmail.com>
2025-02-12 19:57:48 +0100

    Update escoteros.tsx

--- pages/escoteros.tsx
+++ pages/escoteros.tsx
@@ -1,8 +1,8 @@
 import React, { useState, useEffect } from 'react';
-import { Card, CardHeader, CardTitle, CardContent } from '../components/ui/card';
-import { Input } from '../components/ui/input';
-import { Label } from '../components/ui/label';
-import { Button } from '../components/ui/button';
+import { Card, CardHeader, CardTitle, CardContent } from '@/components/ui/card';
+import { Input } from '@/components/ui/input';
+import { Label } from '@/components/ui/label';
+import { Button } from '@/components/ui/button';
 import { AlertCircle } from 'lucide-react';
 
 const Escoteros = () => {
